84c54d2fdc464833042a888ce91fcc125a5397cb
sahil <sahil.v.chaudhari.cg@gmail.com>
2026-03-28 14:36:42 +0530

    some edit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -50,6 +50,8 @@ app.use('/assets', express.static('assets'));
 // Error Handling Middleware
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
+
 if (require.main === module) {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
